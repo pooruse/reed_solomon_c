72ab8526b39971c654c27648bfd756ae2e0990cc
Sheng Wen Peng <pooruse@gmail.com>
2018-07-22 07:47:01 +0800

    Modify tests

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ static uint8_t temp_buffer[NELE];
 static uint8_t msg_buffer[NELE];
 static uint8_t expect[NELE];
 
-void test(char *msg, int len, char *err, int len_err) {
+void test(char *msg, int len, char *err, int len_err, int ret_exp) {
     int i;
     int ret;
     
@@ -21,17 +21,6 @@ void test(char *msg, int len, char *err, int len_err) {
     msg_in.len = len;
     rs_encode_msg(&msg_in);
 
-    printf("msg: ");
-    for(i = 0; i < msg_in.len; i++) {
-	printf("%02X ", msg_in.dat[i]);
-    }
-    printf("\n");
-    printf("err: ");
-    for(i = 0; i < len_err; i++) {
-	printf("%02X ", err[i]);
-    }
-    printf("\n");
-    
     memcpy(expect, msg_in.dat, msg_in.len);
     
     err_in.dat = (uint8_t *)err;
@@ -41,31 +30,19 @@ void test(char *msg, int len, char *err, int len_err) {
     gf_poly_copy(&msg_in, &temp_poly);
     
     ret = rs_decode_msg(&msg_in);
-    
-    if(ret == REED_SOLOMON_NO_ERROR) {
-	printf("No error in the message. \n");
-    } else if(ret == REED_SOLOMON_ERROR_CORRECT) {
-	printf("Error in the message.\n");
-	ret = 0;
+    if(ret == REED_SOLOMON_ERROR_CORRECT || ret == REED_SOLOMON_NO_ERROR) {
 	for(i = 0; i < msg_in.len; i++) {
 	    if(msg_in.dat[i] != expect[i]) {
-		ret = 1;
 		printf("i: %d, expect: %02X, result: %02X\n", i, expect[i], msg_in.dat[i]);
 	    }
 	}
-
-	if(ret) {
-	    printf("Error correct fail.\n");
-	} else {
-	    printf("Error correct success.\n");
-	}
-	
-    } else if(ret == REED_SOLOMON_TOO_MUCH_ERROR) {
-	printf("Too much error.\n");
-    } else {
-	printf("Shouldn't be here.\n");
     }
-    
+    if(ret == ret_exp) {
+	printf("PASS\n");
+	return;
+    } else {
+	printf("ret: %d, ret_exp: %d\n", ret, ret_exp);
+    }
 }
 
 
@@ -79,15 +56,15 @@ int main(void)
     rs_init();
 
     // start test
-    test("01234567", 8, "q\0\0\0\0c\0\0\0\0\0\0", 12);
-    test("01234567", 8, "q\0\0\0\0c\0\0\0\0\0\0", 12);
-    test("01234567", 8, "qb\0\0\0\0\0\0\0\0\0\0", 12);
-    test("basdfeqd", 8, "q\0\0\0\0\0\0\0\0\0\0\0", 12);
-    test("1234kcvj", 8, "q\0\0\0a\0\0\0\0\0\0\0", 12);
-    test("akjviu23", 8, "q\0\0\0\0\0\0e\0\0\0\0", 12);
-    test("ajk38723", 8, "q\0\0\0b\0\0\0\0\0\0\0", 12);
-    test("ajk38723", 8, "q\0\0\0b\0\0\0\0\0e\0", 12);
-    test("ajk38723", 8, "q\0\0\0b\0a\0\0\0e\0", 12);
-    test("ajk38723", 8, "\0\0\0\0\0\0\0\0\0\0\0\0", 12);
+    test("01234567", 8, "q\0\0\0\0c\0\0\0\0\0\0", 12, REED_SOLOMON_ERROR_CORRECT);
+    test("01234567", 8, "q\0\0\0\0c\0\0\0\0\0\0", 12, REED_SOLOMON_ERROR_CORRECT);
+    test("01234567", 8, "qb\0\0\0\0\0\0\0\0\0\0", 12, REED_SOLOMON_ERROR_CORRECT);
+    test("basdfeqd", 8, "q\0\0\0\0\0\0\0\0\0\0\0", 12, REED_SOLOMON_ERROR_CORRECT);
+    test("1234kcvj", 8, "q\0\0\0a\0\0\0\0\0\0\0", 12, REED_SOLOMON_ERROR_CORRECT);
+    test("akjviu23", 8, "q\0\0\0\0\0\0e\0\0\0\0", 12, REED_SOLOMON_ERROR_CORRECT);
+    test("ajk38723", 8, "q\0\0\0b\0\0\0\0\0\0\0", 12, REED_SOLOMON_ERROR_CORRECT);
+    test("ajk38723", 8, "q\0\0\0b\0\0\0\0\0e\0", 12, REED_SOLOMON_TOO_MUCH_ERROR);
+    test("ajk38723", 8, "q\0\0\0b\0a\0\0\0e\0", 12, REED_SOLOMON_TOO_MUCH_ERROR);
+    test("ajk38723", 8, "\0\0\0\0\0\0\0\0\0\0\0\0", 12, REED_SOLOMON_NO_ERROR);
     return 0;
 }
